013d4a82b8c2bdc583490924c75c3e7c2ef9068b
Sreevarsan M <sreevarsan@Sreevarsans-MacBook-Pro.local>
2025-01-31 22:58:21 +0530

    Initial commit

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Sreevarsan",
+  name: "Kishore",
   initials: "M",
   url: "https://dillion.io",
   location: "San Francisco, CA",
